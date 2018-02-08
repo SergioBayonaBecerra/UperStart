1fd08db128d6dfe669701c35be4c426dfdf43299
Sergio Bayona <sergioalejo@gmail.com>
2018-02-08 00:25:25 -0500

    Subimos proyecto inicial en .net core para tener a uperstart como una webapp movemos el codigo anterior a la carpeta htmlui

---
+++ src/WebUI/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebUI
+{
+    class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context => context.Response.WriteAsync("Hello World, from ASP.NET!"));
+        }
+    }
+}
